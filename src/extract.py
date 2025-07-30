f1df18f00cbbda3a310a28fd98c660423b2acad7
Troy <trxoy2@gmail.com>
2025-07-30 18:01:29 -0400

    overwrite

--- src/extract.py
+++ src/extract.py
@@ -42,7 +42,7 @@ def fetch_activity_data():
     after_timestamp = log_human_readable_time(after_timestamp)
     logging.info(f"Fetching activities after: {after_timestamp}")
     # hardcode temporary after timestamp for bulk upload
-    after_timestamp = 1735689600 
+    #after_timestamp = 1735689600 
     params = {
         "after": after_timestamp,
         "per_page": 100
